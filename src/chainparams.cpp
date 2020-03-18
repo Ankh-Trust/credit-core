0eb95b8a0345a47651daa6b41441205071a6e2a9
Mark Sanders <23432027+hmlsanders@users.noreply.github.com>
2020-03-18 18:04:18 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -116,7 +116,7 @@ public:
         strNetworkID = "main";
         consensus.nRewardsStart = 5;               // PoW Rewards begin on block 5
         consensus.nServiceNodePaymentsStartBlock = 64000;  // ServiceNode Payments begin on block 64000
-        consensus.nMinCountServiceNodesPaymentStart = 20;  // ServiceNode Payments begin once 5 ServiceNodes exist or more.
+        consensus.nMinCountServiceNodesPaymentStart = 21;  // ServiceNode Payments begin once 5 ServiceNodes exist or more.
 
         consensus.nInstantSendConfirmationsRequired = 11;
         consensus.nInstantSendKeepLock = 24;
