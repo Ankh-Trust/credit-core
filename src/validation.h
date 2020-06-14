d47e4340d014945ac5add158a93d901599ae8792
hmlsanders <hml.sanders@outlook.com>
2020-06-14 15:27:37 +0200

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -100,7 +100,7 @@ static const unsigned int MAX_HEADERS_RESULTS = 2000;
  *  when requested. For older blocks, a regular BLOCK response will be sent. */
 static const int MAX_CMPCTBLOCK_DEPTH = 5;
 /** Maximum depth of blocks we're willing to respond to GETBLOCKTXN requests for. */
-static const int MAX_BLOCKTXN_DEPTH = 10;
+static const int MAX_BLOCKTXN_DEPTH = 50;
 /** Size of the "block download window": how far ahead of our current height do we fetch?
  *  Larger windows tolerate larger download speed differences between peer, but increase the potential
  *  degree of disordering of blocks on disk (which make reindexing and in the future perhaps pruning
